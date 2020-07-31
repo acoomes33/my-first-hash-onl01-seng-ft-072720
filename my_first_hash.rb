5ba26610ee857b5636fbedf89f96427ba0fce02a
Alan Coomes <acoomes1@cord.edu>
2020-07-31 00:03:30 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,15 +1,16 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-your_hash = {"name" => "Walter White", "occupation" => "science teacher and business owner", "employee" => "Jesse", "favorite color" => "blue"}
+your_hash = {"name" => "Walter White", "occupation" => "science teacher and business owner", "employee" => "Jesse", "favorite_color" => "blue"}
   
 end
 
 
 def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
+  the_manifest = {}
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
+the_manifest
 
 end
 
