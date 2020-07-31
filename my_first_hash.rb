d6dc41ce7f6f5091ec01db3e8b08db2f203c3468
Alan Coomes <acoomes1@cord.edu>
2020-07-31 00:02:03 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+your_hash = {"name" => "Walter White", "occupation" => "science teacher and business owner", "employee" => "Jesse", "favorite color" => "blue"}
   
 end
 
